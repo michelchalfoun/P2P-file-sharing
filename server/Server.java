6b43777dbbbb247b366f9f1aef3a01f5266eb92d
Michel Chalfoun <mikachalfoun@gmail.com>
2021-10-16 23:57:56 -0400

    Setup basic server

--- server/Server.java
+++ server/Server.java
@@ -1,2 +1,115 @@
-package server;public class Server {
+package server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    /*
+    p1 -> listening p2, p3, p4
+
+
+    receive:
+    - handshake
+
+
+    send:
+    - handshake (on handshake or initializes)
+
+The peer that has just started should make TCP connections to all peers that started
+before it. For example, the peer process with peer ID 1003 in the above example should
+make TCP connections to the peer processes with peer ID 1001 and peer ID 1002. In the
+same way, the peer process with peer ID 1004 should make TCP connections to the peer
+processes with peer ID 1001, 1002, and 1003. When all processes have started, all peers
+are connected with all other peers. Note that this behavior is different from that of
+BitTorrent. It is just for simplifying the project.
+
+
+     */
+
+    private static final int sPort = 8000;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running.");
+        ServerSocket listener = new ServerSocket(sPort);
+        int clientNum = 1;
+        try {
+            while(true) {
+                new Server.Handler(listener.accept(),clientNum).start();
+                System.out.println("Client "  + clientNum + " is connected!");
+                clientNum++;
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private String message;    //message received from the client
+        private String MESSAGE;    //uppercase message send to the client
+        private Socket connection;
+        private ObjectInputStream in;	//stream read from the socket
+        private ObjectOutputStream out;    //stream write to the socket
+        private int no;		//The index number of the client
+
+        public Handler(Socket connection, int no) {
+            this.connection = connection;
+            this.no = no;
+        }
+
+        public void run() {
+            try{
+                //initialize Input and Output streams
+                out = new ObjectOutputStream(connection.getOutputStream());
+                out.flush();
+                in = new ObjectInputStream(connection.getInputStream());
+                try{
+                    while(true) {
+                        //receive the message sent from the client
+                        message = (String)in.readObject();
+                        //show the message to the user
+                        System.out.println("Receive message: " + message + " from client " + no);
+                        //Capitalize all letters in the message
+                        MESSAGE = message.toUpperCase();
+                        //send MESSAGE back to the client
+                        sendMessage(MESSAGE);
+                    }
+                }
+                catch(ClassNotFoundException classnot){
+                    System.err.println("Data received in unknown format");
+                }
+            }
+            catch(IOException ioException){
+                System.out.println("Disconnect with Client " + no);
+            }
+            finally{
+                //Close connections
+                try{
+                    in.close();
+                    out.close();
+                    connection.close();
+                }
+                catch(IOException ioException){
+                    System.out.println("Disconnect with Client " + no);
+                }
+            }
+        }
+
+        //send a message to the output stream
+        public void sendMessage(String msg){
+            try{
+                out.writeObject(msg);
+                out.flush();
+                System.out.println("Send message: " + msg + " to Client " + no);
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+
+
+    }
 }
