b7f084c2d33106ff2e5fd179bf1829934583b72d
Michel Chalfoun <mikachalfoun@gmail.com>
2021-10-21 21:19:50 -0400

    Fixed logging

--- src/main/java/Logging/Logging.java
+++ src/main/java/Logging/Logging.java
@@ -13,24 +13,45 @@ public class Logging {
     Logger logger;
     FileHandler fh;
     SimpleFormatter formatter;
-    private final int peerID;
 
-    public Logging(final int peerID) throws IOException {
-        this.peerID = peerID;
+    public Logging() throws IOException {
         this.logger = Logger.getLogger("MyLog");
         this.logger.setUseParentHandlers(false);
-        this.fh = new FileHandler(Paths.get(System.getProperty("user.dir")).getParent().getParent() + "/logs/log_peer_" + peerID + ".log", true);
+    }
+
+    private void setFile(final int peerID_1) throws IOException {
+        //Might need to change this to protect from possible path issues
+        this.fh = new FileHandler(Paths.get(System.getProperty("user.dir")).getParent().getParent() + "/logs/log_peer_" + peerID_1 + ".log", true);
         logger.addHandler(this.fh);
         this.formatter = new SimpleFormatter();
         this.fh.setFormatter(this.formatter);
     }
 
-    public void TCP_connect(final int peerID_2) {
-        this.logger.info("Peer " + this.peerID + " makes a connection to Peer " + peerID_2 + ".");
+    public void TCP_connect(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " makes a connection to Peer " + peerID_2 + ".");
     }
 
-    public void changePreferredNeighbors(final int[] neighborID_list) {
-        String msg = "Peer " + this.peerID + " has the preferred neighbors ";
+    public void TCP_receive(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " is connected from Peer " + peerID_2 + ".");
+    }
+
+    public void changePreferredNeighbors(final int peerID_1, final int[] neighborID_list) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String msg = "Peer " + peerID_1 + " has the preferred neighbors ";
         for (int id : neighborID_list) {
             msg += id + ", ";
         }
@@ -38,35 +59,75 @@ public class Logging {
         this.logger.info(msg.substring(0, msg.length() - 2) + ".");
     }
 
-    public void changeOptimUnchokedNeighbor(final int neighborID) {
-        this.logger.info("Peer " + this.peerID + " has the optimistically unchoked neighbor " + neighborID + ".");
+    public void changeOptimUnchokedNeighbor(final int peerID_1, final int neighborID) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " has the optimistically unchoked neighbor " + neighborID + ".");
     }
 
-    public void unchoking(final int peerID_2) {
-        this.logger.info("Peer " + this.peerID + " is unchoked by " + peerID_2 + ".");
+    public void unchoking(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " is unchoked by " + peerID_2 + ".");
     }
 
-    public void choking(final int peerID_2) {
-        this.logger.info("Peer " + this.peerID + " is choked by " + peerID_2 + ".");
+    public void choking(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " is choked by " + peerID_2 + ".");
     }
 
-    public void receiveHaveMsg(final int peerID_2, final int pieceIndex) {
-        this.logger.info("Peer " + this.peerID + " received the 'have' message from " + peerID_2 + " for the piece " + pieceIndex + ".");
+    public void receiveHaveMsg(final int peerID_1, final int peerID_2, final int pieceIndex) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " received the 'have' message from " + peerID_2 + " for the piece " + pieceIndex + ".");
     }
 
-    public void receiveInterestedMsg(final int peerID_2) {
-        this.logger.info("Peer " + this.peerID + " received the 'interested' message from " + peerID_2 + ".");
+    public void receiveInterestedMsg(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " received the 'interested' message from " + peerID_2 + ".");
     }
 
-    public void receiveNotInterestedMsg(final int peerID_2) {
-        this.logger.info("Peer " + this.peerID + " received the 'not interested' message from " + peerID_2 + ".");
+    public void receiveNotInterestedMsg(final int peerID_1, final int peerID_2) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " received the 'not interested' message from " + peerID_2 + ".");
     }
 
-    public void downloadingPiece(final int peerID_2, final int pieceIndex, final int numOfPieces) {
-        this.logger.info("Peer " + this.peerID + " has downloadded the piece " + pieceIndex + " from " + peerID_2 + ". Now the number of pieces it has is " + numOfPieces + ".");
+    public void downloadingPiece(final int peerID_1, final int peerID_2, final int pieceIndex, final int numOfPieces) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " has downloadded the piece " + pieceIndex + " from " + peerID_2 + ". Now the number of pieces it has is " + numOfPieces + ".");
     }
 
-    public void completionOfDownload() {
-        this.logger.info("Peer " + this.peerID + " has downloaded the complete file");
+    public void completionOfDownload(final int peerID_1) {
+        try {
+            setFile(peerID_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logger.info("Peer " + peerID_1 + " has downloaded the complete file");
     }
 }
