d17f3d08a3f4d9a41ced5dba5cecb65d1cdaa0eb
Michel Chalfoun <mikachalfoun@gmail.com>
2021-12-08 12:54:14 -0500

    Moved tests to test folder

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 mvn clean package
-rm project_tests/project_local_test/peerProcess.jar
-rm project_tests/project_config_file_large/peerProcess.jar
-rm project_tests/project_config_file_small/peerProcess.jar
+rm src/test/project_tests/project_local_test/peerProcess.jar
+rm src/test/project_tests/project_config_file_large/peerProcess.jar
+rm src/test/project_tests/project_config_file_small/peerProcess.jar
 rm transfer/peerProcess.jar
 
-cp target/peerProcess.jar project_tests/project_local_test
-cp target/peerProcess.jar project_tests/project_config_file_large
-cp target/peerProcess.jar project_tests/project_config_file_small
+cp target/peerProcess.jar src/test/project_tests/project_local_test
+cp target/peerProcess.jar src/test/project_tests/project_config_file_large
+cp target/peerProcess.jar src/test/project_tests/project_config_file_small
 cp target/peerProcess.jar transfer/peerProcess.jar
